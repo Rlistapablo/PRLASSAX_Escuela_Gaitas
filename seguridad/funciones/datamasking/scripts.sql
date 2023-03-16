ecbf5c9619d4c0447e3b1ddf3db197cbe9e5aa3a
rlistapablo <rlistapablo@gmail.com>
2023-03-16 01:44:36 +0100

    datamask

--- seguridad/funciones/datamasking/scripts.sql
+++ seguridad/funciones/datamasking/scripts.sql
@@ -11,3 +11,70 @@ CREATE TABLE alum_tar(
     CVC CHAR(3) NOT NULL,
     Caducidad DATE NOT NULL
 )
+DROP USER IF EXISTS Administracion
+GO 
+CREATE USER Administracion WITHOUT LOGIN
+GO 
+GRANT SELECT ON alum_tar TO Administracion
+GO
+CREATE OR ALTER PROCEDURE mascaras
+AS
+BEGIN
+SELECT c.name, tbl.name as table_name, c.is_masked, c.masking_function FROM sys.masked_columns 
+AS c JOIN sys.tables AS tbl ON c.object_id = tbl.object_id
+WHERE is_masked = 1;
+END
+GO
+--DEFAULT
+ALTER TABLE alum_tar
+ALTER COLUMN CVC CHAR(3) MASKED WITH (FUNCTION = 'default()')
+GO
+EXECUTE AS USER = 'Administracion'
+GO
+PRINT USER
+GO
+SELECT * FROM alum_tar
+GO
+REVERT 
+GO
+--PARTIAL
+ALTER TABLE alum_tar
+ALTER COLUMN Numero VARCHAR(50) MASKED WITH (FUNCTION = 'partial(0,"---------",5)')
+GO
+EXECUTE AS USER = 'Administracion'
+GO
+PRINT USER
+GO
+SELECT * FROM alum_tar
+GO
+REVERT 
+GO
+--RANDOM
+ALTER TABLE alum_tar
+ALTER COLUMN CVC int MASKED WITH (FUNCTION = 'random(0,999)')
+GO
+EXECUTE AS USER = 'Administracion'
+GO
+PRINT USER
+GO
+SELECT * FROM alum_tar
+GO
+REVERT 
+GO
+--DATE
+ALTER TABLE alum_tar
+ALTER COLUMN Caducidad ADD MASKED WITH (FUNCTION = 'datetime("Y")')
+GO
+EXECUTE AS USER = 'Administracion'
+GO
+PRINT USER
+GO
+SELECT * FROM alum_tar
+GO
+REVERT 
+GO
+--UNMASK
+GRANT UNMASK TO Administracion
+GO 
+REVOKE UNMASK TO Administracion
+GO
